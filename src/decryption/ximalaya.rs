148389eb03b8367cec5b3c37d975ad40c6ea1c33
Jixun Wu <jixun.moe@gmail.com>
2022-05-28 19:59:34 +0100

    feat: add x3m test

--- src/decryption/ximalaya.rs
+++ src/decryption/ximalaya.rs
@@ -11,7 +11,7 @@ pub type ScrambleTable = [u16; XMLY_SCRAMBLE_SIZE];
 
 enum State {
     DecryptHeader,
-    Passthrough,
+    PassThrough,
 }
 
 pub struct Ximalaya<T> {
@@ -59,10 +59,10 @@ impl<const KEY_SIZE: usize> Decryptor for Ximalaya<[u8; KEY_SIZE]> {
                 State::DecryptHeader => {
                     if self.data.read_until_offset(&mut p, XMLY_SCRAMBLE_SIZE) {
                         self.do_header_decryption();
-                        self.state = State::Passthrough;
+                        self.state = State::PassThrough;
                     }
                 }
-                State::Passthrough => {
+                State::PassThrough => {
                     self.data.buf_out.extend_from_slice(p);
                     break;
                 }
@@ -78,15 +78,14 @@ pub type XimalayaX3M = Ximalaya<X3MContentKey>;
 
 #[cfg(test)]
 pub mod test {
-    use super::{ScrambleTable, XimalayaX2M, XMLY_SCRAMBLE_SIZE};
+    use super::{ScrambleTable, XimalayaX2M, XimalayaX3M, XMLY_SCRAMBLE_SIZE};
     use crate::{
-        decryption::ximalaya::X2MContentKeySize,
+        decryption::ximalaya::{X2MContentKeySize, X3MContentKeySize},
         utils::test_util::test::{decrypt_test_content, generate_test_data, TEST_SIZE_1MB},
     };
-    use std::convert::TryInto;
 
     #[test]
-    fn test_xmly() {
+    fn test_x2m() {
         let test_data = generate_test_data(TEST_SIZE_1MB, "x2m-test-data");
         let x2m_content_key = generate_test_data(X2MContentKeySize, "x2m content key");
 
@@ -117,6 +116,41 @@ pub mod test {
         assert_eq!(
             result,
             "fd1ac1c4750f48b8d3c9562013f1c3202b12e45137b344995eda32a4f6b8a61f"
+        );
+    }
+
+    #[test]
+    fn test_x3m() {
+        let test_data = generate_test_data(TEST_SIZE_1MB, "x3m-test-data");
+        let x3m_content_key = generate_test_data(X3MContentKeySize, "x3m content key");
+
+        let mut x3m_scramble_table: ScrambleTable = [0u16; XMLY_SCRAMBLE_SIZE];
+        for (i, v) in x3m_scramble_table.iter_mut().enumerate() {
+            *v = i as u16;
+        }
+        let table_size = x3m_scramble_table.len();
+        let x3m_scramble_seed = generate_test_data(table_size * 2, "x3m seed");
+        for i in 0..table_size {
+            let n = u16::from_le_bytes(
+                x3m_scramble_seed[i * 2..i * 2 + 2]
+                    .try_into()
+                    .expect("incorrect length"),
+            ) as usize;
+            x3m_scramble_table.swap(i, n % table_size);
+        }
+
+        assert_eq!(x3m_content_key.len(), X3MContentKeySize);
+
+        let mut decryptor = XimalayaX3M::new(
+            x3m_content_key
+                .try_into()
+                .expect("could not format to array"),
+            x3m_scramble_table,
+        );
+        let result = decrypt_test_content(&mut decryptor, test_data.as_ref());
+        assert_eq!(
+            result,
+            "a10bbfdcdbd388373361da6baf35c80b725f7310c3eca29d7dcf228e397a8c5a"
         );
     }
 }
