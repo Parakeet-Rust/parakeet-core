23729071df58b503ac2e6f7df7bbc167f3f669eb
Jixun Wu(@shen) <jixun.moe@gmail.com>
2022-06-08 21:52:03 +0100

    chore: explicitly re-export qmc methods

--- src/decryption/mod.rs
+++ src/decryption/mod.rs
@@ -1,10 +1,11 @@
 pub mod kugou;
 pub mod kuwo;
 pub mod ncm;
-pub mod qmc;
 pub mod ximalaya;
 
+mod qmc;
 mod qmc_v1;
 mod qmc_v2;
 
+pub use qmc::new_qmc_v2;
 pub use qmc_v1::new_qmc_v1_static as new_qmc_v1;
