f031c529c47787a1063c2fdd34053622865fc962
Jixun Wu(@shen) <jixun.moe@gmail.com>
2022-06-08 21:53:36 +0100

    chore: re-export all vendor new methods

--- src/decryption/mod.rs
+++ src/decryption/mod.rs
@@ -7,5 +7,20 @@ mod qmc;
 mod qmc_v1;
 mod qmc_v2;
 
+// Kugou
+pub use kugou::new_kgm;
+pub use kugou::new_vpr;
+
+// Kuwo
+pub use kuwo::new_kwm;
+
+// Netease
+pub use ncm::new_ncm;
+
+// Tencent QQMusic (QMC)
 pub use qmc::new_qmc_v2;
 pub use qmc_v1::new_qmc_v1_static as new_qmc_v1;
+
+// Ximalaya
+pub use ximalaya::new_x2m;
+pub use ximalaya::new_x3m;
